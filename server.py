aa1caa57e5de79316a526953c08c09ecc7fd99b6
sofu0185 <sofu0185@edu.easj.dk>
2019-05-15 22:10:26 +0200

    Python UDP socket server

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+from time import sleep
+
+serverName = '192.168.0.255'
+serverPort = 12000
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as clientSocket:
+    count = 0
+    while True:
+        clientSocket.sendto(str(count).encode(), (serverName, serverPort))
+        count += 1
+        sleep(5)
