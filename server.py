da2ed015f7d1fc9f0b3bbf46c623d06e08b898ce
sofu0185 <sofu0185@edu.easj.dk>
2019-05-15 22:45:04 +0200

    Added socket options for linux

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ serverName = '192.168.0.255'
 serverPort = 12000
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as clientSocket:
+    clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     count = 0
     while True:
         clientSocket.sendto(str(count).encode(), (serverName, serverPort))
