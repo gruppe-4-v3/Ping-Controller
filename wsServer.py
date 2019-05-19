5059f6fd8de3d7042234b3076c2e371e2e85d7e3
sofu0185 <sofu0185@edu.easj.dk>
2019-05-19 17:46:40 +0200

    Fix - Now displaying the right ip on linux/raspberry pi

--- wsServer.py
+++ wsServer.py
@@ -4,14 +4,16 @@ import asyncio
 import websockets
 import socket
 
-localIp = socket.gethostbyname(socket.gethostname())
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+localIp = s.getsockname()[0]
+
 print('Local ip:', localIp)
 
 async def main(websocket, path):
     # Logic inside this method
     count = 0
     while True:
-        
         await websocket.send(str(count))
         await asyncio.sleep(1)
         count = count + 1
@@ -21,3 +23,6 @@ start_server = websockets.serve(main, localIp, 12000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+
+
+
