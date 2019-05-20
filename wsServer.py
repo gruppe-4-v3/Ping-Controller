6f5e4198a1de5a10328e3e4549c850722ae1456d
sofu0185 <sofu0185@edu.easj.dk>
2019-05-20 14:51:58 +0200

    Start sending data by clicking joystick

--- wsServer.py
+++ wsServer.py
@@ -3,6 +3,7 @@
 import asyncio
 import websockets
 import socket
+from sense_hat import SenseHat, ACTION_PRESSED
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
@@ -10,14 +11,27 @@ localIp = s.getsockname()[0]
 
 print('Local ip:', localIp)
 
+sense = SenseHat()
+
+gameInProgress = False
+
+def middle_Click(event):
+    global gameInProgress
+    if event.direction == "middle" & event.action == ACTION_PRESSED:
+        gameInProgress = True
+
+sense.stick.direction_middle = middle_Click
+
 async def main(websocket, path):
     # Logic inside this method
-    count = 0
-    while True:
-        await websocket.send(str(count))
-        await asyncio.sleep(1)
-        count = count + 1
+    global gameInProgress
 
+    while True:
+        middle_Click(sense.stick.wait_for_event(True))
+        
+        while gameInProgress:
+            await asyncio.sleep(1)
+    
 
 start_server = websockets.serve(main, localIp, 12000)
 
