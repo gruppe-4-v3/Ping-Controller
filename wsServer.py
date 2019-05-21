cba2dcefde07ac87453672815cec4a3fc28fd02a
sofu0185 <sofu0185@edu.easj.dk>
2019-05-21 10:05:09 +0200

    now sends data to website

--- wsServer.py
+++ wsServer.py
@@ -4,6 +4,7 @@ import asyncio
 import websockets
 import socket
 from sense_hat import SenseHat, ACTION_PRESSED
+from time import sleep
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
@@ -26,13 +27,43 @@ sense.stick.direction_middle = middle_Click
 async def main(websocket, path):
     # Logic inside this method
     global gameInProgress
+    global sense
 
     while True:
-        event = sense.stick.wait_for_event()
-        if event.direction == "middle":
-            while gameInProgress:
-                print("test")
-                await asyncio.sleep(1)
+        sense.stick.wait_for_event()
+        sleep(.5)
+
+        speed = 0
+        red = (255, 0, 0)
+
+        while gameInProgress:
+            acceleration = sense.get_accelerometer_raw()
+            x = acceleration['x']
+            
+            speed *= 0.9
+        
+            if (x > 0.1 or x < -0.1):
+                speed += x
+            
+            print("speed:", speed)
+            websocket.send(speed)
+
+            sense.clear()
+            if (speed > 0.5 and speed < 1):
+                sense.set_pixel(2, 5, red)
+            if (speed > 1 and speed < 2):
+                sense.set_pixel(1, 5, red)
+            if (speed > 2):
+                sense.set_pixel(0, 5, red)
+            if (speed < -0.5 and speed > -1):
+                sense.set_pixel(5, 5, red)
+            if (speed < -1 and speed > -2):
+                sense.set_pixel(6, 5, red)
+            if (speed < -2):
+                sense.set_pixel(7, 5, red)
+            if (speed < 0.5 and speed > -0.5):
+                sense.set_pixel(4, 5, red)
+                sense.set_pixel(3, 5, red)
 
 
 start_server = websockets.serve(main, localIp, 12000)
